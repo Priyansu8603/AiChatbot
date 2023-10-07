9b1da39f2b49211e4f08df222944c67d02d9a6ba
Priyansu8603 <priyanshukumar238202@gmail.com>
2023-10-07 23:48:12 +0530

    Adding Splash Screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.aichatbot"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.aichatbot"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -38,12 +38,14 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-auth-ktx:22.1.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    implementation("com.airbnb.android:lottie:6.1.0")
+
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
--- app/src/main/java/com/example/aichatbot/MainActivity.kt
+++ app/src/main/java/com/example/aichatbot/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.aichatbot
 
+import android.content.Intent
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startHeavyTask()
+    }
+
+    private fun startHeavyTask() {
+        LongOperation().execute()
+    }
+    private open inner class LongOperation : AsyncTask<String?, Void, String?>(){
+        override fun doInBackground(vararg p0: String?): String? {
+            for(i in 0..6){
+                try {
+                    Thread.sleep(1000)
+                }
+                catch (e: Exception){
+                    Thread.interrupted()
+                }
+            }
+            return "result"
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            val intent = Intent(this@MainActivity,homeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
